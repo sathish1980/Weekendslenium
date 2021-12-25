6fee4edc5a36dcd592612bdcef3e9d2dd85cec88
sathish1980 <kumar.sathish189@gmail.com>
2021-12-25 09:58:30 +0530

    sample test java file created

---
+++ src/test/java/PCK1/WeekendSeleniumBatch/Sampletest.java
@@ -0,0 +1,19 @@
+package PCK1.WeekendSeleniumBatch;
+
+public class Sampletest {
+	
+	public void add()
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sampletest s = new Sampletest();
+		s.add();
+	}
+
+}
