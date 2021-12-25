747b6a74e70511deab56bb2189eb8b82ed433965
sathish1980 <kumar.sathish189@gmail.com>
2021-12-25 10:09:20 +0530

    mul method added in sampletest .java

--- src/test/java/PCK1/WeekendSeleniumBatch/Sampletest.java
+++ src/test/java/PCK1/WeekendSeleniumBatch/Sampletest.java
@@ -9,11 +9,19 @@ public class Sampletest {
 		int c=a+b;
 		System.out.println(c);
 	}
+	public void mul()
+	{
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println(c);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Sampletest s = new Sampletest();
 		s.add();
+		s.mul();
 	}
 
 }
