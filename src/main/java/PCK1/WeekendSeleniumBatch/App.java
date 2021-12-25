1f0d1e257c5854dad1ba7a112721e0c2ec8a9621
sathish1980 <kumar.sathish189@gmail.com>
2021-12-25 09:50:22 +0530

    new project creation

---
+++ src/main/java/PCK1/WeekendSeleniumBatch/App.java
@@ -0,0 +1,13 @@
+package PCK1.WeekendSeleniumBatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
